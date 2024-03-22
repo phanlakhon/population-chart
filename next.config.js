e591422f401dee5b0765a8466ce5bb9ecaab9a90
Oorathai Phanlakhon <phanlakhon@Oorathais-Mac-mini.local>
2024-03-22 10:50:30 +0700

    Fix deploy

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/population-chart",
+  assetPrefix: "/population-chart/",
   trailingSlash: true,
   output: "export",
   reactStrictMode: true,
   // images: {
   //   unoptimized: true,
   // },
-  basePath: "/population-chart",
-  assetPrefix: "/population-chart",
-  async rewrites() {
-    return [
-      {
-        source: "/population-chart/:path*",
-        destination: "/:path*", // The :path parameter is used here so will not be automatically passed in the query object.
-      },
-    ];
-  },
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/population-chart/:path*",
+  //       destination: "/:path*", // The :path parameter is used here so will not be automatically passed in the query object.
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
